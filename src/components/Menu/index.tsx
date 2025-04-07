3566b669170fa9e4c979a2506b6e1987077ce0dd
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-04-07 08:08:14 -0300

    Theme Light and Dark created

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,19 +1,60 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
 import styles from './styles.module.css';
+import { useState } from 'react';
+import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
+  const [theme, setTheme] = useState<AvailableThemes>('dark');
+
+  function HandleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    console.log('Clicado', Date.now());
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+  document.documentElement.setAttribute('data-theme', theme);
+
   return (
     <div className={styles.menu}>
-      <a className={styles.menuLink} href='#'>
+      <h1>{theme}</h1>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para Home'
+        title='Ir para Home'
+      >
         <HouseIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver histórico'
+        title='Ver histórico'
+      >
         <HistoryIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar tema'
+        title='Mudar tema'
+        onClick={HandleThemeChange}
+      >
         <SunIcon />
       </a>
     </div>
