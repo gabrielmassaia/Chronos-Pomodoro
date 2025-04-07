079b914a5eb5a3cdaf5ce4b105ae2a465be937e9
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-04-07 08:21:21 -0300

    linkedin vinculado corretamente

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export function Footer() {
   return (
     <footer className={styles.footer}>
       <a href=''>Entenda a técnica pomodoro</a>
-      <a href='https://www.linkedin.com/in/gabriel-massaia-8710b1222/https://www.linkedin.com/notifications/?filter=all'>
+      <a href='https://www.linkedin.com/in/gabriel-massaia-8710b1222/'>
         Chronos Pomodoro &copy; {new Date().getFullYear()}
       </a>
     </footer>
