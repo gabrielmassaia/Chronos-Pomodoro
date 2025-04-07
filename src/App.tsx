145b728446698c4110032f4ec3a6804e99a9db41
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-04-06 21:02:42 -0300

    Component Footer Created

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { CountDown } from './components/CountDown';
 import { DefaultInput } from './components/DefaultInput';
 import { Cycles } from './components/Cycles';
 import { DefaultButton } from './components/DefaultButton';
+import { Footer } from './components/Footer';
+import { PlayCircleIcon } from 'lucide-react';
 
 import './styles/theme.css';
 import './styles/global.css';
@@ -40,9 +42,12 @@ export function App() {
           <div className='formRow'></div>
           <Cycles />
           <div className='formRow'>
-            <DefaultButton icon='Qualquer Coisa' />
+            <DefaultButton icon={<PlayCircleIcon />} color='green' />
           </div>
         </form>
+      </Container>
+      <Container>
+        <Footer />
       </Container>
     </>
   );
