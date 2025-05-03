cb29b075ca393a8e54f184b9e87ba334bb9605da
Gabriel Massaia <gabrielmassaia50@gmail.com>
2025-05-03 15:23:19 -0300

    cria mapeamento de estados

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -4,14 +4,16 @@ import { DefaultButton } from '../DefaultButton';
 import { DefaultInput } from '../DefaultInput';
 import { useRef } from 'react';
 import { TaskModel } from '../../models/TaskModel';
+import { useTaskContext } from '../../contexts/TaskContext/UseTaskContext';
 
 export function MainForm() {
+  const { setState } = useTaskContext();
   const taskNameInput = useRef<HTMLInputElement>(null);
 
   function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
-    if (taskNameInput === null) return;
+    if (taskNameInput.current === null) return;
 
     const taskName = taskNameInput.current.value.trim();
     console.log(taskName);
@@ -20,7 +22,29 @@ export function MainForm() {
       alert('Digite algo no input');
     }
 
-    const newTask: TaskModel = {};
+    const newTask: TaskModel = {
+      id: Date.now().toString(),
+      name: taskName,
+      startDate: Date.now(),
+      completeDate: null,
+      interruptDate: null,
+      duration: 1,
+      type: 'workTime',
+    };
+
+    const secondsRemaining = newTask.duration * 60;
+
+    setState(prevState => {
+      return {
+        ...prevState,
+        config: { ...prevState.config },
+        activeTask: newTask,
+        currentCycle: 1,
+        secondsRemaining,
+        formattedSecondsRemaining: '00:00',
+        tasks: [...prevState.tasks, newTask],
+      };
+    });
   }
   return (
     <form onSubmit={handleCreateNewTask} className='form' action=''>
